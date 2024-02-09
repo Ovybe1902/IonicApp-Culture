68da6193f9c0dfb69ca62000980e140d762a505a
Aro Andrianimanana <aroandrianimanana@gmail.com>
2024-02-09 23:38:20 +0300

    User Profile

--- src/components/CHeader/HeaderApp.tsx
+++ src/components/CHeader/HeaderApp.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { IonRow, IonCol, IonText, IonIcon, IonButton, IonHeader, IonModal, IonContent, IonList, IonItem, IonImg } from '@ionic/react';
 import { notificationsOutline, personOutline, closeOutline, createOutline } from 'ionicons/icons';
 import NotificationCard from '../CNotificationItem/NotificationCard';
+import "./HeaderApp.css";
 
 interface CHeaderProps {
   notifications: any[];
@@ -66,7 +67,48 @@ const HeaderApp: React.FC<CHeaderProps> = ({ notifications }) => {
       {/* User Menu Modal */}
       <IonModal ref={userMenuModalRef} onDidDismiss={closeModals}>
         <IonContent>
-          {/* Your user menu content */}
+        <div className="user-profile">
+          <IonButton onClick={closeModals} fill="clear" slot="end">
+            <IonIcon icon={closeOutline} />
+          </IonButton>
+          <IonRow className="profile-pic-row ion-align-items-center ion-justify-content-center">
+            <div className="image-container">
+              <IonImg className="profile-pic" src="/assets/image/johnDoe.jpg" alt="Profile Pic" />
+            </div> 
+          </IonRow>
+          <div className="basic-info">
+            <IonRow className="username-row ion-align-items-center ion-justify-content-center">
+              <div className="username-group">
+                <IonText className="username" style={{ fontSize: '32px', fontWeight: 'bold' }}>John Doe</IonText>
+                <IonImg className="certified-icon" src="/assets/icons/certified.png" alt="Certified" />
+              </div>
+            </IonRow>
+            <IonRow className="email-row ion-align-items-center ion-justify-content-center">
+              <IonText className="email-address" style={{ fontSize: '16px', fontWeight: '600', opacity: '0.6' }}>johndoe@gmail.com</IonText>
+            </IonRow>
+          </div>
+          <IonRow>
+            <IonCol size='6'>
+              <div className="user-info surface">
+                <h1>45621 ha</h1>
+                <p>owned surface</p>
+              </div>
+            </IonCol>
+            <IonCol size='6'>
+              <div className="user-info plot">
+                <h1>4512</h1>
+                <p>plot number</p>
+              </div>
+            </IonCol>
+          </IonRow>
+          <IonRow className="disconnect-button-row ion-align-items-center ion-justify-content-center">
+            <IonButton className="edit-button" style={{ backgroundColor: 'rgba(231, 255, 226, 0.592)' }} >
+            <IonIcon icon={createOutline} />
+              Edit
+            </IonButton>
+            <IonButton className="disconnect-button" href='/login'>Disconnect</IonButton>
+          </IonRow>
+        </div>
         </IonContent>
       </IonModal>
     </IonHeader>
